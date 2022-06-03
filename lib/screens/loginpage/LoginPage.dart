c1c07b11f27bff47ed67744723db8713bccc906e
Sahil Sharma <74351253+ys1113457623@users.noreply.github.com>
2022-06-03 09:21:47 -0700

    Update LoginPage.dart

--- lib/screens/loginpage/LoginPage.dart
+++ lib/screens/loginpage/LoginPage.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:foodify/utils/theme.dart';
 
-
 import 'InputWrapper.dart';
 
 class LoginPage extends StatelessWidget {
-
-  
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
@@ -28,37 +25,45 @@ class LoginPage extends StatelessWidget {
       },
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Stack(
-          children: 
-            [Container(
-              width: double.infinity * 1.5,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/loginpage_backgroundimage.jpg"),fit: BoxFit.cover
+        body: LayoutBuilder(
+          builder: (context, constraint) {
+            return Stack(
+              children: [
+                Container(
+                  width: double.infinity * 1,
+                  height: double.infinity * 0.5,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            "assets/images/loginpage_backgroundimage.jpg"),
+                        fit: BoxFit.cover),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: screenSize.height * 0.27,
+                      ),
+                      Expanded(
+                          child: SingleChildScrollView(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: (isLightMode
+                                  ? lightThemeData(context).bottomAppBarColor
+                                  : darkThemeData(context)
+                                      .scaffoldBackgroundColor),
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              )),
+                          child: InputWrapper(),
+                        ),
+                      )),
+                    ],
+                  ),
                 ),
-              ),
-              
-              child: Column(
-                children: <Widget>[
-                  
-                   SizedBox(height: screenSize.height * 0.27,),
-                  
-                  Expanded(child: Container(
-                    decoration: BoxDecoration(
-                        color: (isLightMode ? lightThemeData(context).scaffoldBackgroundColor : darkThemeData(context).scaffoldBackgroundColor),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        )
-                    ),
-                    child: InputWrapper(),
-                  )),
-                  
-                ],
-              ),
-            ),
-            
-          ],
+              ],
+            );
+          },
         ),
       ),
     );
